81a06bc070d9a29cd102e2b3aa1b2fa76d29cf6d
J123123-oss <a2723406795@gmail.com>
2025-07-07 19:03:55 +0800

    add auto .sh

---
+++ autostart.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#开启CAN转USB
+sudo chmod 777 /dev/ttyACM3
+sudo slcand -o -c -s8 /dev/ttyACM3 can0
+sudo ip link set can0 type can bitrate 1000000
+sudo ifconfig can0 up
+
+sudo chmod 777 /dev/ttyUSB0
+# sudo chmod 777 /dev/ttyACM3
+
+#香橙派设置
+# cd /home/orangepi/demo01 
+#虚拟机设置
+cd /home/ubuntu/demo01
+
+source devel/setup.bash
+roslaunch motor_can run.launch
+
+
+
+#结束后需要关闭CAN
+# sudo ifconfig can0 down
