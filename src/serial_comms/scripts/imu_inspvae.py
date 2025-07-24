587b40d3ef89477ff5e40c59462dc7dcc3115539
J123123-oss <a2723406795@gmail.com>
2025-07-24 14:23:32 +0800

    change ttyUSB0 to IMU

--- src/serial_comms/scripts/imu_inspvae.py
+++ src/serial_comms/scripts/imu_inspvae.py
@@ -13,7 +13,7 @@ class IMUParser:
         rospy.init_node('imu_parser_node')
         
         # 参数配置
-        self.port = rospy.get_param('~serial_port', '/dev/ttyUSB0')
+        self.port = rospy.get_param('~serial_port', '/dev/IMU')
         self.baudrate = rospy.get_param('~baudrate', 115200)
         self.device_addr = 0x50
         self.rx_frame_length = 7
