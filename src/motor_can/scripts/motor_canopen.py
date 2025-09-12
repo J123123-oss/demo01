6b85dba2da71bc934acfe67539fd495a3160e4df
J123123-oss <a2723406795@gmail.com>
2025-09-12 13:59:24 +0800

    sleep 60 plan

--- src/motor_can/scripts/motor_canopen.py
+++ src/motor_can/scripts/motor_canopen.py
@@ -18,8 +18,8 @@ import select
 rate = 68  # Hz   166.66>> 68.26
 
 class ServoDriveController:
-    def __init__(self, channel='vcan0', interface='socketcan'):
-    # def __init__(self, channel='can0', interface='socketcan'):
+    # def __init__(self, channel='vcan0', interface='socketcan'):
+    def __init__(self, channel='can0', interface='socketcan'):
         self.bus = can.interface.Bus(channel=channel, interface=interface)
         self.last_left_speed = 0
         self.last_right_speed = 0
@@ -496,8 +496,8 @@ class ServoDriveController:
         self.bus.shutdown()
 
     @staticmethod
-    # def load_config(config_file="/home/orangepi/demo01/src/motor_can/config/servo_config.yaml"):
-    def load_config(config_file="/home/ubuntu/demo01/src/motor_can/config/servo_config.yaml"):
+    def load_config(config_file="/home/orangepi/demo01/src/motor_can/config/servo_config.yaml"):
+    # def load_config(config_file="/home/ubuntu/demo01/src/motor_can/config/servo_config.yaml"):
         try:
             with open(config_file, 'r') as file:
                 config = yaml.safe_load(file)
