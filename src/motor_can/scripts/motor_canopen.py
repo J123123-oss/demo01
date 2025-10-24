19fc9a61080fcdb73f08d62356bb3f8552f1fd6f
J123123-oss <a2723406795@gmail.com>
2025-10-24 09:47:20 +0800

    disable fault code in STOP

--- src/motor_can/scripts/motor_canopen.py
+++ src/motor_can/scripts/motor_canopen.py
@@ -1416,9 +1416,9 @@ class ServoDriveController:
                         rospy.loginfo(f"电机 {motor_id} 无故障")
                         self.motor_driver = True
                     return fault_code
-        rospy.logwarn(f"读取电机 {motor_id} 故障码超时")
-        self.motor_driver = False
-
+        if self.current_status != "STOP":
+            self.motor_driver = False
+            rospy.logwarn(f"读取电机 {motor_id} 故障码超时")
         return None
 
     def clear_fault(self, motor_id):
