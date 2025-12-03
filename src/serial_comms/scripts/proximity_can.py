9d139de2c1fea6765c3e57c228ba120e716dc713
J123123-oss <a2723406795@gmail.com>
2025-12-03 13:56:27 +0800

    change proximity can reinit

--- src/serial_comms/scripts/proximity_can.py
+++ src/serial_comms/scripts/proximity_can.py
@@ -149,9 +149,9 @@ class DigitalInputReader:
 
     def poll_inputs(self, event):
         """Query and process digital inputs"""
-        if self.bus is None:
-            rospy.logerr("CAN bus not initialized!")
-            return
+        # if self.bus is None:
+        #     rospy.logerr("CAN bus not initialized!")
+        #     return
             
         try:
             query_frame = self._create_query_frame()
